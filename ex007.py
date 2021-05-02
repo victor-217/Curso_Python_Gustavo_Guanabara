57eb03cd59566de65b4f17a15ac2c12f832ab3df
Victor Augusto <victoraugusto1933@gmail.com>
2021-05-02 11:46:31 -0300

    Update ex007.py

--- ex007.py
+++ ex007.py
@@ -1,4 +1,7 @@
-print('='*5, 'Média arimetmetica', '='*5)
+'''
+MÉDIA ARITMÉTICA: Desenvolva  um programa que leia as duas notas de um aluno,
+calcule e mostre a sua média.
+'''
 
 nota1 = float(input('Informe a primeira nota do aluno: '))
 nota2 = float(input('Segundo nota do aluno: '))
