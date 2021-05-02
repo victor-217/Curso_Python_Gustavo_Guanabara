3ebf2312107d3e2d5481674e7cc894f1fe1fe240
Victor Augusto <victoraugusto1933@gmail.com>
2021-05-02 12:07:23 -0300

    Update ex016.py

--- ex016.py
+++ ex016.py
@@ -1,3 +1,8 @@
+'''
+quebrando numero: Crie um programa que leia um número real qualquer pelo teclado e mostre na tela a sua porcao inteira
+ex: digite um numero: 6.127 o número 6.127 tem a parte inteira 6
+'''
+
 from math import trunc
 
 num = float(input('Digite um número: '))
