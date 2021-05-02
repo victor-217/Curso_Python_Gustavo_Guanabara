6373d1a3d5de4432ba5a8860b31c5fc904f9e235
Victor Augusto <victoraugusto1933@gmail.com>
2021-05-02 12:02:13 -0300

    Update ex014.py

--- ex014.py
+++ ex014.py
@@ -1,3 +1,8 @@
+'''
+CONVERSOR DE TEMPERATURA  : Escreva um programa que converta uma temperatura
+digitada em °C e converta em °F.
+'''
+
 temp = float(input('Informe a temperatura em °C: '))
 f = (temp * 9 / 5) + 32
 print('A temperatura de {}°C corresponde a {}°F'.format(temp, f))
