8f45e015783e767e9be980a912d98c959b705a50
Victor Augusto <victoraugusto1933@gmail.com>
2021-05-02 13:25:44 -0300

    Update ex020.py

--- ex020.py
+++ ex020.py
@@ -1,3 +1,8 @@
+'''
+O mesmo professor do desafio anterior quer sortear a ordem de apresentação de trabalho dos alunos.
+Faça um programa que leia o nome dos quatro alunos e mostre a ordem de sorteada.
+'''
+
 from random import shuffle
 
 a1 = str(input('Primeiro aluno: ').title())
