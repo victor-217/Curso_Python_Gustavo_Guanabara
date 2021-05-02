fce4649178846640f6da8c4d4cb75747233438bc
Victor Augusto <victoraugusto1933@gmail.com>
2021-05-02 11:42:01 -0300

    Update ex005.py

--- ex005.py
+++ ex005.py
@@ -1,4 +1,8 @@
-print('='*10, 'Descubrindo o Antecessor e Sucessor', '='*10)
+'''
+ANTECESSOR E SUCESSOR: Faça um programa que leia um número inteiro e mostre na tela seu
+antecessor e seu sucessor.
+'''
+
 num = int(input('Digite um número: '))
 print('\nAntecessor de {} é {}'.format(num, num - 1))
 print('Sucessor de {} é {}'.format(num, num + 1))
