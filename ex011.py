a794f501a5e088a597c2366e571aed1818d18ab4
Victor Augusto <victoraugusto1933@gmail.com>
2021-05-02 11:57:11 -0300

    Update ex011.py

--- ex011.py
+++ ex011.py
@@ -1,4 +1,8 @@
-print('='*5, 'Pintando a parede', '='*5)
+'''
+PINTANDO PAREDE: Faça um programa que leia a largura e a altura de uma parede em metros, calcule a sua área e a quantidade
+de tinta nescessária para pintá-la, sabendo que cada litro de tinta pinta uma área de 2m².
+'''
+
 lar = float(input('Qual a largura da parede? (mt)'))
 alt = float(input('Qual a altura da parede? (mt)'))
 area = lar * alt
