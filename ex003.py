a2016bf3cc82a2b8a2fdfed696b90afeee186800
Victor Augusto <victoraugusto1933@gmail.com>
2021-05-02 11:32:53 -0300

    Update ex003.py

--- ex003.py
+++ ex003.py
@@ -1,4 +1,6 @@
-print('='*10, 'ex 003', '='*10)
+'''
+Crie um programa que leia o nome de uma pessoa e mostre na tela uma mensagem de boas vindas!
+'''
 n1 = int(input('Digite um número: '))
 n2 = int(input('Digite outro número: '))
 print('\nA soma entre {} é {} é igual a {}!'.format(n1, n2, n1+n2))
