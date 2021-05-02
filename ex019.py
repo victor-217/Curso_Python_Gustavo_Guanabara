ea4c276f05017056b028d0a7d694738caa71bf8a
Victor Augusto <victoraugusto1933@gmail.com>
2021-05-02 13:23:46 -0300

    Update ex019.py

--- ex019.py
+++ ex019.py
@@ -1,3 +1,8 @@
+'''
+SORTEANDO ITEM NA LISTA: Um professor quer sortear um dos seus quatro alunos para apagar o quadro.
+Faça um programa que ajude ele, lendo o nome deles e escrevendo o nome do escolhido.
+'''
+
 from random import choice
 
 a1 = input('Primeiro aluno: ').title()
