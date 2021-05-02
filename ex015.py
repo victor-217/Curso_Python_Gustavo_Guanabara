e78c53af4dda305fa16bb9f9d75a7c00d3757c93
Victor Augusto <victoraugusto1933@gmail.com>
2021-05-02 12:05:28 -0300

    Update ex015.py

--- ex015.py
+++ ex015.py
@@ -1,3 +1,9 @@
+'''
+ALUGUEL DE CARROS: Escreva um programa que pergunte a quantidade de km percorridos por
+um carro alugado e a quantidade de dias pelo quais ele foi alugado.
+Calcule o preço a pagar, sabendo que o carro custa R$60 por dia e R$0,15 por km rodado
+'''
+
 dias = int(input('Quantos dias o carro ficou alugado? '))
 Km = float(input('Quantos Km rodados? '))
 pagar = (dias * 60) + (Km * 0.15)
