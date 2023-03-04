0f3050edd7c15a0a7d732a3a1cb326a854caf3ed
Victor Augusto <victoraugusto1933@gmail.com>
2023-03-04 14:29:23 +0000

    Update ex004.py

--- ex004.py
+++ ex004.py
@@ -1,14 +1,13 @@
-'''
-DISSECANDO UMA VARIÁVEL: Faça um programa que leia algo pelo  teclado e mostre na tela seu tipo
-primitivo e todas as informações possíveis sobre ele.
-'''
+"""EX004: Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo
+primitivo e todas as informações possíveis sobre ele."""
 
-algo = input('Digite algo: ')
-print('\nTipo primitivo {}'.format(type(algo)))
-print('Só tem espaços? {}'.format(algo.isspace()))
-print('É Alfabético? {}'.format(algo.isalpha()))
-print('É alfanumérico? {}'.format(algo.isalnum()))
-print('É um número? {}'.format(algo.isnumeric()))
-print('Está em maiúsculo? {}'.format(algo.isupper()))
-print('Está em minúsculo? {}'.format(algo.islower()))
-print('Está capitalizada? {}'.format(algo.istitle()))
+algo = input('>>> Digite algo: ')
+
+print(f'\n>> O seu tipo primitivo é {type(algo)}')
+print(f'>> Só tem espaços? {algo.isspace()}')
+print(f'>> É alfanúmerico? {algo.isalnum()}')
+print(f'>> É alfabético? {algo.isalpha()}')
+print(f'>> É um número? {algo.isnumeric()}')
+print(f'>> Está em maiúsculas? {algo.isupper()}')
+print(f'>> Está em minúsculas? {algo.islower()}')
+print(f'>> Está capitalizada? {algo.istitle()}')
