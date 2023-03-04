7b9bea9e83a0ccc576cd183b71d1ec6c1e1d2e18
Victor Augusto <victoraugusto1933@gmail.com>
2023-03-04 15:00:24 +0000

    Update ex013.py

--- ex013.py
+++ ex013.py
@@ -1,8 +1,8 @@
-'''
-REAJUSTE SALARIAL: Faça um algoritmo que leia o salário de um funcionário e mostre
-seu novo salário, com 15% de aumento
-'''
+"""EX013: Faça um algoritmo que leia o salário de um funcionário e mostre seu
+novo salário, com 15% de aumento."""
 
-sl = float(input('Qual valor do salário do funcionário? R$'))
-aumento = sl + (sl*15 / 100)
-print('\nO funcionário que ganhava R${:.2f}, com 15% de aumento, passa a receber R${:.2f}'.format(sl, aumento))
+salario = float(input('>>> Qual valor do salário do funcionário? R$'))
+
+aumento = salario*15/100
+
+print(f'\n>> Com aumento de 15%, o funcionário que ganhava R${salario} passará a ganhar R${salario+aumento:.2}')
