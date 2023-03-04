806a865c719bead99a4b5c7147d30aac503b04cf
Victor Augusto <victoraugusto1933@gmail.com>
2023-03-04 15:13:24 +0000

    Update ex016.py

--- ex016.py
+++ ex016.py
@@ -1,9 +1,8 @@
-'''
-quebrando numero: Crie um programa que leia um número real qualquer pelo teclado e mostre na tela a sua porcao inteira
-ex: digite um número: 6.127 o número 6.127 tem a parte inteira 6
-'''
+"""EX016: Crie um programa que leia um número real qualquer pelo teclado e mostre na tela a sua porcao inteira
+ex: digite um número: 6.127 o número 6.127 tem a parte inteira 6"""
 
 from math import trunc
 
-num = float(input('Digite um número: '))
-print('O número digítado foi {} e sua porção inteira é {}'.format(num, trunc(num)))
+num = float(input('>>> Digite um número: '))
+
+print(f'>> O número digítado foi {num} e sua parte inteira é {trunc(num)}.')
