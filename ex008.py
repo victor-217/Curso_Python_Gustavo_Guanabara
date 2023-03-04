903f120730480a02e5c599259613a4de42b8943d
Victor Augusto <victoraugusto1933@gmail.com>
2023-03-04 14:39:21 +0000

    Update ex008.py

--- ex008.py
+++ ex008.py
@@ -1,19 +1,18 @@
-'''
-CONVERSOR DE MEDIDAS : Escreva um programa que leia um valor em metros e o exiba
-convertidos em centrimetros e milímetros.
-'''
+"""EX008: Escreva um programa que leia um valor em metros e o exiba em centímetros e milímetros."""
 
 mt = float(input('Uma distância em metros:  '))
+
 km = mt / 1000
 hm = mt / 100
 dam = mt / 10
 dm = mt * 10
 cm = mt * 100
 mm = mt * 1000
-print('A medida de {}m corresponde a'.format(mt))
-print('{}Km'.format(km))
-print('{}hm'.format(hm))
-print('{}dam'.format(dam))
-print('{:.0f}dm'.format(dm))
-print('{:.0f}cm'.format(cm))
-print('{:.0f}mm'.format(mm))
+
+print(f'A medida de {mt}m corresponde a')
+print(f'{km}Km')
+print(f'{hm}hm')
+print(f'{dm}dam')
+print(f'{dm:.0f}dm')
+print(f'{cm:.0f}cm')
+print(f'{mm:.0f}mm')
