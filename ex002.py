fef1123b593f2ce5cefaa0e0ffa4b45af705dfc1
Victor Augusto <victoraugusto1933@gmail.com>
2023-03-04 14:22:26 +0000

    Update ex002.py

--- ex002.py
+++ ex002.py
@@ -1,6 +1,5 @@
-'''
-Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas!
-'''
+"""Ex002: Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas."""
 
-nome = input('Digite seu nome: ')
-print('É um prazer te conhecer, {}!'.format(nome))
+nome = input(str('>>> Qual o seu nome? '))
+
+print(f'\n>> Olá {nome.title()}, seja bem vindo!')
