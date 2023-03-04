8466bf9a58abaecdf1c5151d96519efeefe02e93
Victor Augusto <victoraugusto1933@gmail.com>
2023-03-04 14:50:17 +0000

    Update ex010.py

--- ex010.py
+++ ex010.py
@@ -1,12 +1,14 @@
-'''
-CONVERSOR DE MOEDAS: Crie um programa que leia quanto dinheiro uma pessoa tem na carteira
-e mostre quantos dólares ela pode comprar.
-'''
+"""EX010: Crie um programa que leia quanto dinheiro a pessoa tem na carteira e
+mostre quantos Dólares ela pode comprar."""
+
+print('\033[95m====== CONVERSOR DE MOEDA ======\033[m')
 
 real = float(input('Quanto você tem na carteira? (R$)'))
+
 dl = real / 5.42
 euro = real / 6.42
 iene = real / 19.31
-print('\nCom R${:.2f} você pode comprar US${:.2f}'.format(real, dl))
-print('Com R${:.2f} você pode comprar €{:.2f}euros'.format(real, euro))
-print('Com R${:.2f} você pode comprar ¥{:.2f}ienes'.format(real, iene))
+
+print(f'\nCom R${real:.2f} você pode comprar US${dl:.2f}')
+print(f'Com R${real:.2f} você pode comprar €{euro:.2f}')
+print(f'Com R${real:.2f} você pode comprar ¥{iene:.2f}')
