9d9299d2f4a965bd2f9f8863a0564163654eb74e
Victor Augusto <victoraugusto1933@gmail.com>
2023-03-04 14:40:20 +0000

    Update ex009.py

--- ex009.py
+++ ex009.py
@@ -1,18 +1,16 @@
-'''
-TABUADA: Faça um programa que leia um número inteiro qualquer e mostre na tela sua tabuada
-'''
+""""EX009: Faça um programa que leia um número Inteiro qualquer e mostre na tela sua tabuada."""
 
-n = int(input('Digite um valor para ver sua tabuada:'))
-print('='*14)
-print('{} x  0 = {}'.format(n, n*0))
-print('{} x  1 = {}'.format(n, n*1))
-print('{} x  2 = {}'.format(n, n*2))
-print('{} x  3 = {}'.format(n, n*3))
-print('{} x  4 = {}'.format(n, n*4))
-print('{} x  5 = {}'.format(n, n*5))
-print('{} x  6 = {}'.format(n, n*6))
-print('{} x  7 = {}'.format(n, n*7))
-print('{} x  8 = {}'.format(n, n*8))
-print('{} x  9 = {}'.format(n, n*9))
-print('{} x 10 = {}'.format(n, n*10))
-print('='*14)
+n = int(input('>>> Digite um número para ver sua tabuada: '))
+
+print('=='*10)
+print(f'{n} x {1:2} = {n*1}')
+print(f'{n} x {2:2} = {n*2}')
+print(f'{n} x {3:2} = {n*3}')
+print(f'{n} x {4:2} = {n*4}')
+print(f'{n} x {5:2} = {n*5}')
+print(f'{n} x {6:2} = {n*6}')
+print(f'{n} x {7:2} = {n*7}')
+print(f'{n} x {8:2} = {n*8}')
+print(f'{n} x {9:2} = {n*9}')
+print(f'{n} x {10:2} = {n*10}')
+print('=='*10)
