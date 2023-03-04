f1eef6483724e2adcdf630278b188953ee1a7a41
Victor Augusto <victoraugusto1933@gmail.com>
2023-03-04 15:29:36 +0000

    Update ex018.py

--- ex018.py
+++ ex018.py
@@ -1,12 +1,12 @@
-'''
-SENO, COSSENO, TANGENTE: Faça um programa que leia um ângulo qualquer
-e mostre na tela o valor do seno, cosseno e tangente desse ângulo.
-'''
+"""EX018: Faca um programa que leia um angulo qualquer e mostre na tela o valor do seno, cosseno e da
+tangente desse angulo."""
 
 from math import cos, sin, tan, radians
 
-ang = float(input('Digite o ângulo desejado: '))
-rad = radians(ang)
-print('\nO ângulo de {} tem o SENO {:.2f}'.format(ang, sin(rad)))
-print('O ângulo de {} tem o COSSENO {:.2f}'.format(ang, cos(rad)))
-print('O ângulo de {} tem a TANGENTE {:.2f}'.format(ang, tan(rad)))
+angulo = float(input('>>> Digite o ângulo desejado: '))
+
+rad = radians(angulo)
+
+print(f'\n>> O ângulo de {angulo} tem o SENO {sin(rad):.2f}')
+print(f'>> O ângulo de {angulo} tem o COSSENO {cos(rad):.2f}')
+print(f'>> O ângulo de {angulo} tem a TANGENTE {tan(rad):.2f}')
