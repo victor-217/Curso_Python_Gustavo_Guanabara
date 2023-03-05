8d694375c2b8818cd31c52720f4a8e6cc8748c98
Victor Augusto <victoraugusto1933@gmail.com>
2023-03-05 11:10:52 +0000

    Update ex024.py

--- ex024.py
+++ ex024.py
@@ -1,7 +1,8 @@
-"""
-Crie um programa que leia o nome de uma cidade e diga se ela começa ou não com o nome 'SANTO'.
-"""
+"""EX024: Crie um programa que leia o nome de uma cidade e diga se ela
+começa ou não com o nome 'SANTO'."""
 
-cidade = str(input('Em que cidade você mora? ')).lower()
-separa = cidade.split()
-print('A cidade começa com Santo? {}'.format('santo' in separa[0]))
+nome = str(input('>>> Digite o nome de uma cidade: '))
+
+separar = nome.upper().split()
+
+print(f'\n>> A cidade de {nome.title()} começa com "Santo"? {"SANTO" in separar[0]}')
