93507199ebfa6e77a50962273c3e0baec8d4b61e
Victor Augusto <victoraugusto1933@gmail.com>
2023-03-05 10:58:46 +0000

    Update ex023.py

--- ex023.py
+++ ex023.py
@@ -1,14 +1,11 @@
-"""
-Faça um programa que leia um número de 0 a 9999 e mostre na tela cada um dos digitos separados.
+"""EX023: Faça um programa que leia um número de 0 a 9999 e mostre na tela cada um dos digitos separados.
 ex:Digite um número:1111
-unidade:
-dezena:
-centena:
-milhar:
-"""
-num = input('Digite um número: ').zfill(4)
-print('Analizando...')
-print('\nUnidade: {}'.format(num[3]))
-print('Dezena: {}'.format(num[2]))
-print('Centena: {}'.format(num[1]))
-print('Milhar: {}'.format(num[0]))
+unidade: 1, dezena: 1, centena: 1, milhar:1 """
+
+num = input('>>> Digite um número: ').zfill(4)
+
+print('>> Analizando...')
+print(f'\n> Unidade: {num[3]}')
+print(f'> Dezena: {num[2]}')
+print(f'> Centena: {num[1]}')
+print(f'> Milhar: {num[0]}')
