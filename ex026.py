ca1991a0fd1d196dab764aa53efa6a170c5d8ca5
Victor Augusto <victoraugusto1933@gmail.com>
2023-03-06 10:36:34 +0000

    Update ex026.py

--- ex026.py
+++ ex026.py
@@ -1,11 +1,10 @@
-"""
-Faça um programa que leia uma frase pelo teclado e mostre na tela:
-* Quantas vezes aparece a letra 'A'
+"""EX026: Faça um programa que leia uma frase pelo teclado e mostre na tela:
+* Quantas vezes aparece a letra "A"
 * Em que posição ela aparece a primeira vez
-* Em que posição ela aparece a última vez
-"""
+* Em que posição ela aparece a última vez"""
 
-frase = str(input('Digite uma frase: ')).strip().upper()
-print('\nA letra "A" aparece {} vezes.'.format(frase.count('A')))
-print('A letra "A" aparece a primeira vez na posição {}.'.format(frase.find('A')+1))
-print('A letra "A" aparece pela última vez na posição {}.'.format(frase.rfind('A')+1))
+frase = str(input('>>> Digite uma frase: ')).strip().upper()
+
+print(f'\n>> A letra "A" aparece {frase.count("A")} vezes.')
+print(f'>> A letra "A" aparece a primeira vez na posição {frase.find("A")+1}.')
+print(f'>> A letra "A" aparece pela última vez na posição {frase.rfind("A")+1}.')
