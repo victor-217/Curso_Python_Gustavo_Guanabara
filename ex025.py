9036cb8944e937cd28be7a3c09608fc8be3afa19
Victor Augusto <victoraugusto1933@gmail.com>
2023-03-06 10:30:02 +0000

    Update ex025.py

--- ex025.py
+++ ex025.py
@@ -1,6 +1,5 @@
-"""
-Crie um programa que leia o nome de uma pessoa e diga se ela tem 'SILVA' no nome.
-"""
+"""EX025: Crie um programa que leia o nome de uma pessoa e diga se
+ela tem 'SILVA' no nome."""
 
-nome = str(input('Qual seu nome completo? ')).lower()
-print('Seu nome tem Silva? {}'.format('silva' in nome))
+nome = str(input('>>> Digite seu nome completo: ')).lower()
+print(f'>> Seu nome tem "SILVA"? {"silva" in nome}')
