20b5b1e485bd216d47ad24e94052ae414ac81928
Victor Augusto <victoraugusto1933@gmail.com>
2023-03-08 09:50:05 +0000

    Update ex027.py

--- ex027.py
+++ ex027.py
@@ -1,9 +1,9 @@
-"""
-Faça um programa que leia o nome completo de uma pessoa,
-mostrando em seguida o primeiro e o último nome separadamente.
-"""
+"""EX027: Faça um programa que leia o nome completo de uma pessoa,
+mostrando em seguida o primeiro e o último nome separadamente."""
 
-nome = input('Digite seu nome completo: ').strip().title()
-separa = nome.split()
-print('\nSeu primeiro nome é {}'.format(separa[0]))
-print('Seu último nome é {}'.format(separa[-1]))
+nome = str(input('>>> Digite seu nome completo: ').strip())
+
+separar = nome.split()
+
+print(f'\n>> Seu primeiro nome é {separar[0].title()}')
+print(f'>> Seu último nome é {separar[-1].title()}')
